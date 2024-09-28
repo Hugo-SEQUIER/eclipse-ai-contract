e728606a424e34248525357906f491d1ef4a1abb
Hugo Sequier <hugosequier@gmail.com>
2024-09-28 23:36:21 +0300

    contract

--- eclipse-ai-contract/programs/eclipse-ai-contract/src/lib.rs
+++ eclipse-ai-contract/programs/eclipse-ai-contract/src/lib.rs
@@ -6,11 +6,15 @@ declare_id!("GsmQ5nMCAveArB81aJ4G3U1QTkXhayPkMecyfQpMhZpu");
 pub mod eclipse_ai_contract {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn sign_access(ctx: Context<SignAccess>) -> Result<()> {
+        // This function doesn't need to do anything other than succeed
+        // The mere fact that it was called and succeeded is enough to grant access
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct SignAccess<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
